67aa632fd0933efe08def81eb57074f5f06b3c37
Ian Hoyos <ianhoyos@Ians-MacBook-Pro.local>
2017-05-08 13:28:44 -0400

    second commit

--- submit.php
+++ submit.php
@@ -6,6 +6,18 @@ ini_set('display_errors', 1);
 
  //Garambaproject@gmail.com:universityofmiami
  //garambaproject:University1
+ 
+ function is_connected(){
+    $connected = @fsockopen("www.google.com", 80); 
+    if ($connected){
+        $is_conn = true; //action when connected
+        fclose($connected);
+    }else{
+        $is_conn = false; //action in connection failure
+    }
+    return $is_conn;
+
+}
 
 $latitude = $_POST['latitude'];
 $longitude = $_POST['longitude'];
@@ -14,6 +26,17 @@ $type = $_POST['type'];
 $reporter = $_POST['reporter'];
 $comment = $_POST['comment'];
 
-mysqli_query($db, "INSERT INTO `garamba` (`id`,`latitude`,`longitude`,`type`,`date`,`reporter`,`comment`) VALUES 
+for($_POST as $post) $string .= "$post,";
+
+if(is_connected()){
+  mysqli_query($db, "INSERT INTO `garamba` (`id`,`latitude`,`longitude`,`type`,`date`,`reporter`,`comment`) VALUES 
                 (NULL,'$latitude','$longitude','$type',STR_TO_DATE('$dates','%m/%d/%Y'),'$reporter','$comment')");
+  }else{
+   
+     $file = fopen("backup.txt","w")  or die("Unable to open file!");
+     fwrite($file,$string);
+     fclose($file);
+   
+   
+  }
 ?>
